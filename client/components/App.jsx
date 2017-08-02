5d54eded0f370b2410e7c91d7c4d1ef83a56846c
joshua-fogg <joshuafoggnz@gmail.com>
2017-08-02 12:09:35 +1200

    building app

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const App = () => {
-  return (
-    <h1>React development has begun!</h1>
-  )
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      quote: "",
+    }
+    this.setQuote = this.setQuote.bind(this)
+  }
+  handleOnClick() {
+    getQuote(setQuote){
+      apicall()
+
+    }
+
+  }
+
+  render() {
+    return (
+      <div id='page'>
+        <div className='title'><h1> Tronald Dump Quote Generator</h1></div>
+        <div className='trump-button'>
+          <button onClick={handleOnClick}>
+            <g src='./ronaldo-dumpster.svg' />
+          </button>
+        </div>
+        <div className='quote'>
+          <p>
+            {this.state.quote}
+          </p>
+        </div>
+
+      </div >
+    )
+  }
 }
 
 export default App
